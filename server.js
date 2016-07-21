e39836131633d2e866dea5bffa0558fdf226b1fb
Rey Garcia <reynaldogarcia@Reynaldos-iMac.local>
2016-07-20 21:29:54 -0500

    edit

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
+var app = express();
+
+app.use(jsonParser);
 
 var Storage = function() {
     this.items = [];
@@ -18,31 +21,48 @@ var storage = new Storage();
 storage.add('Broad beans');
 storage.add('Tomatoes');
 storage.add('Peppers');
-storage.add('shits');
 
-var app = express();
+
 app.use(express.static('public'));
 
 app.get('/items/', function(req, res) {
     res.json(storage.items);
 });
 
+//app post
+app.post('/items', jsonParser, function (req, res) {
 
-app.post
-app.post('/items', jsonParser, function(req, res) {
-    if (!req.body) {
-        return res.sendStatus(400);
+    if (req.body) {
+        var item = storage.add(req.body.name);
+        res.status(201).json(item);
+    } else {
+        res.sendStatus(400);
     }
-
-    var item = storage.add(req.body.name);
-    res.status(201).json(item);
 });
 //app.put
+app.put('/items/:id', function (req, res) {
 
+    var id = parseInt(req.params.id);
+    var name = req.body.name;
+
+    for (var i = 0; i < storage.items.length; i++) {
+        if (storage.items[i].id === id) {
+            storage.items[i].name = name;
+            res.status(201).json(storage.items);
+        }
+    }
+});
 //app.delete
+app.delete('/items/:id', function (req, res) {
 
-
-
+    var id = parseInt(req.params.id);
+    for (var i = 0; i < storage.items.length; i++) {
+        if (storage.items[i].id === id) {
+            storage.items.splice(i, 1);
+            res.status(201).json(storage.items);
+        }
+    }
+});
 
 app.listen(process.env.PORT || 8080, function (){
 	console.log('servers is running on port' + 8080)
